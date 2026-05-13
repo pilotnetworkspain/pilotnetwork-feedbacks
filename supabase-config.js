7890d78261464a26a46ddc2e40fd0aaf359f5f4e
pilotnetworkspain <pilotnetworkspain@gmail.com>
2026-05-13 12:45:43 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.PN_SUPABASE_CONFIG = {
-  SUPABASE_URL: "https://qmouukexrgmjrphonuhg.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtb3V1a2V4cmdtanJwaG9udWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MTUxMzMsImV4cCI6MjA5NDE5MTEzM30.OZiLu1ltKgiMdOdlekupHeIjd4Bl0xWDZlf9ro_wD54",
+  SUPABASE_URL: "https://jnaldsbrbauydbetzpqn.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpuYWxkc2JyYmF1eWRiZXR6cHFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MTYxNjAsImV4cCI6MjA5NDE5MjE2MH0.alluo1d0_qPYx5c_artvYXcccs5mGSNz-lxQFK9IPYY",
   STORAGE_BUCKET: "feedback-files",
   MAX_FILE_SIZE_BYTES: 10485760,
   MAX_FILES_PER_FEEDBACK: 5,
